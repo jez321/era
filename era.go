1d3d159e28a39104987cd2f18ae37036f0401976
jez321 <jezjez@gmail.com>
2022-05-03 10:48:34 +0900

    fix comments

--- era.go
+++ era.go
@@ -2,38 +2,40 @@
 // and additional key/value data.
 //
 // Usage:
-//    // Creating an error
-//    if err := doSomething(x, y); err != nil {
-//	  	return era.New(fmt.Errorf("doing something: %w", err),
-//			era.WithCode(EInternalError),
-//			era.WithMessage("An internal error occured."),
-//			era.WithFields(era.F{ "x": x, "y": y }),
-//		)
-//    }
 //
-//   // Retrieving the custom data
-//   code := era.Code(err)
-//   msg := era.Message(err)
-//   fields := era.Fields(err)
+//		// Creating an error
+//		if err := doSomething(x, y); err != nil {
+//			return era.New(fmt.Errorf("doing something: %w", err),
+//				era.WithCode(EInternalError),
+//				era.WithMessage("An internal error occured."),
+//				era.WithFields(era.F{ "x": x, "y": y }),
+//			)
+//		}
+//
+//		// Retrieving the custom data
+//		code := era.Code(err)
+//		msg := era.Message(err)
+//		fields := era.Fields(err)
 //
 // When an error is wrapped multiple times with era, Code() and Message() will return the outermost code and message,
 // so you can overwrite this data further up the call stack where you may have more context.
-//   // In your service layer
-//   return era.Error(
-//		fmt.Errorf("checking password: %w", err),
-//		era.WithCode(EInvalidPassword),
-//	 )
 //
-//	 // In your handler (assuming you return an error to a wrapping error handler that then
-//	 // extracts and returns the message)
-//   if err := svc.Login(user, pw); err != nil {
-//	   err = fmt.Errorf("logging in: %w", err)
-//     fldOpt := era.WithFields(era.F{ "user": user })
-//	   if era.Code(err) == EInvalidPassword {
-//	   		return era.New(err, era.WithMessage("Invalid login credentials."), fldOpt)
-//     }
-//	   return era.New(err, era.WithMessage("Internal error."), fldOpt)
-//   }
+//		// In your service layer
+//		return era.Error(
+//			fmt.Errorf("checking password: %w", err),
+//			era.WithCode(EInvalidPassword),
+//		)
+//
+//		// In your handler (assuming you return an error to a wrapping error handler that then
+//		// extracts and returns the message)
+//		if err := svc.Login(user, pw); err != nil {
+//			err = fmt.Errorf("logging in: %w", err)
+//			fldOpt := era.WithFields(era.F{ "user": user })
+//			if era.Code(err) == EInvalidPassword {
+//				return era.New(err, era.WithMessage("Invalid login credentials."), fldOpt)
+//			}
+//			return era.New(err, era.WithMessage("Internal error."), fldOpt)
+//		}
 //
 // Field data from multiple wrapper era errors is combined, with data from outermost errors taking precedence
 // if the same key exists more than once.
@@ -50,9 +52,11 @@ type Option interface {
 }
 
 // Option represents multiple era error options using for creating default sets of options.
+//
 // Usage:
-//	opts := era.Options{era.WithCode("abc"), era.WithMessage("def")}
-//	err := era.New(errors.New("my error"), opts)
+//
+//		opts := era.Options{era.WithCode("abc"), era.WithMessage("def")}
+//		err := era.New(errors.New("my error"), opts)
 type Options []Option
 
 func (o Options) apply(e *eraError) {
